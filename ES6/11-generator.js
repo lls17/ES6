6281edb921edbf982a34019027c75c096f5f143f
lilisha <lls17@qq.com>
2017-07-04 16:17:11 +0800

    笔记补充

--- ES6/11-generator.js
+++ ES6/11-generator.js
@@ -1,3 +1,13 @@
+/**
+ * Generator 是个函数，遍历器，状态机
+ * 
+ *   ① 函数名字前面有 *
+ *   ② 函数内部使用 yield语句
+ * 
+ * 		遍历器：  for-of只能遍历有遍历器的对象
+ * 			返回是个对象，有next()方法，方法返回value和done
+ * */
+
 function* show(){
 	yield "apple";
 	yield "banana";
